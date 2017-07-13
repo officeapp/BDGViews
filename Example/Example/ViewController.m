6f723547a1142657c1a77f8dce09c9319e51000f
Roy Derks <roy@synappz.nl>
2017-07-13 13:14:40 +0200

    Added BDGTabsView to the POD.

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -13,6 +13,29 @@
 #import "BDGShadowedView.h"
 #import "BDGRoundedButton.h"
 #import "BDGDashedLineView.h"
+#import "BDGTabsView.h"
+
+@interface Tag : NSObject
+
+@property(nonatomic,strong) NSString *name;
+
+-(instancetype)initWithName:(NSString*)name;
+
+@end
+
+@implementation Tag
+
+-(instancetype)initWithName:(NSString *)name
+{
+    self = [super init];
+    if(!self) {
+        return nil;
+    }
+    _name = name;
+    return self;
+}
+
+@end
 
 @interface ViewController ()
 {
@@ -25,6 +48,9 @@
 @property(nonatomic,weak) IBOutlet BDGShadowedView *shadowedView;
 @property(nonatomic,weak) IBOutlet BDGRoundedButton *roundedButton;
 @property(nonatomic,weak) IBOutlet BDGDashedLineView *dashedLineView;
+@property(nonatomic,weak) IBOutlet BDGTabsView *tabFilterView;
+
+@property(nonatomic,strong) NSMutableArray *tags;
 
 @end
 
@@ -57,6 +83,27 @@
     
     self.dashedLineView.dashedGap = 10.0f;
     self.dashedLineView.dashedLength = 2.0f;
+    
+    self.tags = [NSMutableArray new];
+    for(int i = 1; i < 11; i++) {
+        [self.tags addObject:[[Tag alloc] initWithName:[NSString stringWithFormat:@"Tag %d", i]]];
+    }
+    
+    self.tabFilterView.lineHeight = 2;
+    self.tabFilterView.upperCase = TRUE;
+    self.tabFilterView.disableScrolling = false;
+    self.tabFilterView.tagPropertyName = @"name";
+    self.tabFilterView.activeColor = [UIColor whiteColor];
+    self.tabFilterView.lineColor = [UIColor yellowColor];
+    self.tabFilterView.inactiveColor = [UIColor darkGrayColor];
+    self.tabFilterView.backgroundColor = [UIColor lightGrayColor];
+    self.tabFilterView.tagFont = [UIFont systemFontOfSize:14.0f];
+    self.tabFilterView.paddingSides = 20.0;
+    self.tabFilterView.paddingBetween = 10.0;
+    [self.tabFilterView setTagSelected:^(id tag) {
+        NSLog(@"Tag selected!");
+    }];
+    self.tabFilterView.tags = self.tags;
 }
 
 
